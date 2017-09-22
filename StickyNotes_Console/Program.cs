b4a61366679d1b7e7c893bbb0df3e384094980e2
vyong <vyong@udel.edu>
2017-09-22 17:40:53 -0400

    Added an example deserialize

--- StickyNotes_Console/Program.cs
+++ StickyNotes_Console/Program.cs
@@ -13,6 +13,20 @@ namespace StickyNotes_Console
     {
         static void Main(string[] args)
         {
+            //Establish the directory
+            string name = @"MyAssignments.xml";
+            string path = Environment.CurrentDirectory;
+            path = System.IO.Path.GetFullPath(System.IO.Path.Combine(path, @"..\..\")); //Move 2 folders up
+            path = System.IO.Path.Combine(path, name);
+
+            //Ser(path);
+            DeSer(path);
+            
+            Console.ReadLine();
+        }
+
+        public static void Ser(string path)
+        {
             List<Course> courses = new List<Course>();
 
             courses.Add(new Course("CPEG 250", new List<Assignment>() {
@@ -23,20 +37,30 @@ namespace StickyNotes_Console
             courses.Add(new Course("HAHA 101", new List<Assignment>() {
                 new Assignment("Aeschluss", DateTime.Now),
             }));
-
-            //Establish the directory
-            string name = @"MyAssignments.xml";
-            string path = Environment.CurrentDirectory;
-            path = System.IO.Path.GetFullPath(System.IO.Path.Combine(path, @"..\..\")); //Move 2 folders up
-            path = System.IO.Path.Combine(path, name);
-
+            
             //Serialize
             XmlSerializer serializer = new XmlSerializer(typeof(List<Course>));
             StreamWriter writer = new StreamWriter(path);
             serializer.Serialize(writer, courses);
+        }
 
+        public static void DeSer(string path)
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(List<Course>));
+            FileStream readStream = new FileStream(path, FileMode.Open);
 
-            Console.ReadLine();
+            List<Course> courses;
+            courses = deserializer.Deserialize(readStream) as List<Course>;
+            readStream.Close();
+
+            foreach(Course course in courses)
+            {
+                Console.WriteLine(course.ToString());
+                foreach(Assignment assignment in course.Assignments)
+                {
+                    Console.WriteLine(assignment.ToString());
+                }
+            }
         }
     }
 }
