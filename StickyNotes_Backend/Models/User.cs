68237cad980e1a308d5a1fbe2d77f02eb31e7b0f
vyong <vyong@udel.edu>
2017-10-10 21:07:38 -0400

    Added basis for User class

---
+++ StickyNotes_Backend/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StickyNoteApplication.Models
+{
+    public class User
+    {
+        public List<Course> Courses { get; set; }
+        public string PreviousFilePath { get; set; }
+        private const string UserDataFilePath = @"";
+
+        public User()
+        {
+
+        }
+
+        public void SerializeUser()
+        {
+
+        }
+
+        public void DeserializeUser()
+        {
+
+        }
+    }
+}
